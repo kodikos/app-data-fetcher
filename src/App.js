5f2e5bc01924a88a9fa65267154f9791a7de982d
jodiw <jodi.winters@bbc.co.uk>
2020-12-21 19:22:00 +0000

    Got skeleton in, just refreshing to go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+function ContentFetcher({ prefix, setPrefix })  {
+  const [ content, setContent ] = useState({ path: 'N/A', time: 'N/A'});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://localhost:3020${prefix}`, {
+        method: 'POST',
+        mode: 'cors'
+      });
+      const body = await response.json();
+      setContent(body);
+    }
+    fetchData();
+  }, [prefix]);
+
+  return (
+    <>
+      <div>Path: {content.path}</div>
+      <div>Time: {content.time}</div>
+      <div>Also see: <button onClick={() => setPrefix(content.nextUrl)}>{content.nextUrl}</button></div>
+    </>
+  );
+}
+
+function SettingsForm({ onChange, prefix }) {
+  const [ fieldPrefix, setFieldPrefix ] = useState(prefix);
+
+  useEffect(() => {
+    setFieldPrefix(prefix);
+  }, [prefix]);
+
+  return <form onSubmit={(e) => e.preventDefault()}>
+    <label>Prefix <input type="text" value={fieldPrefix} onChange={(e) => setFieldPrefix(e.target.value)} /></label>
+    <button onClick={() => {
+      onChange(fieldPrefix);
+    }}>Go!</button>
+  </form>;
+}
+
 function App() {
+
+  const [ prefix, setPrefix ] = useState("/initial");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SettingsForm onChange={(newVal) => setPrefix(newVal)} prefix={prefix} />
+      <ContentFetcher prefix={prefix} setPrefix={(newPre) => setPrefix(newPre)} />
     </div>
   );
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors =  require('cors');
+
+const randomUrlList = ['/binky', '/blobby', '/neural'];
 
 function serve(port) {
     const app = express();
-
-    app.use((req, res) => {
+    app.use(cors(), (req, res, next) => {
+        const filteredRandomList = randomUrlList.filter((url) => url !== req.originalUrl);
         res.send({
             path: req.originalUrl,
-            time: (new Date()).toISOString()
+            time: (new Date()).toISOString(),
+            nextUrl: filteredRandomList[Math.floor(Math.random() * filteredRandomList.length)]
         });
+        next();
     });
 
     app.listen(port, () => {
